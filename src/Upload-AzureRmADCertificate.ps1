d24382f091233008a750ac6e42618cb6dabbebd9
Degant Puri <degant7@gmail.com>
2017-04-08 03:22:04 +0530

    WIP

--- src/Upload-AzureRmADCertificate.ps1
+++ src/Upload-AzureRmADCertificate.ps1
@@ -1,41 +1,45 @@
-###############################################################################
-# Task         : Upload Certificate to Azure AD resource
-# Description  : Upload a local certificate file (.cer or .pfx) to an Azure AD
-#                resource. Checks whether the Azure AD resource already contains
-#				 a certificate with the same end date and doesn't upload if that
-#				 is true. The End Date is used as a comparision mechanism because
-#				 currently the Powershell cmdlet doesn't have the capability to 
-#				 return the thumbprint, which would be a better solution.
-# Version      : 1.0.0
-# Author       : Degant Puri
-# Input        : -Path  
-#                    Local file system Path of certificate 
-#                -ApplicationId
-#                    Application Id of Azure AD resource
-###############################################################################
+#########################################################################################################################
+# Task         :	Upload Certificate to Azure AD resource
+# Description  :	Upload a local certificate file (.cer or .pfx) to an Azure AD resource if it isn't already present. 
+#					Validates whether the Azure AD resource already contains the same certificate based on the end date.
+#
+#					Ideally the certificate thumbprint would be the right approach for comparison but currently the 
+#					Powershell cmdlet 'Get-AzureRmADAppCredential' doesn't have the ability to return the thumbprint. So 
+#					the end date comparision is used as a workaround.
+#
+# Version      :	1.0.0
+# Author       :	Degant Puri
+# Input        :	-Path  
+#						Local file system Path of certificate 
+#					-ApplicationId
+#						Application Id of Azure AD resource
+#########################################################################################################################
 
 param (
 	[string] $Path,
-    [string] $Password,
+	[string] $Password,
 	[string] $ApplicationId
 )
 
-Write-Host '[verbose] Loading certificate from path "'$Path'"'
-$cer = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 
-$cer.Import($Path, $Password, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::PersistKeySet) 
-$binCert = $cer.GetRawCertData() 
-$credValue = [System.Convert]::ToBase64String($binCert)
+Write-Host '[verbose] Loading certificate from path:' $Path
 
-$binCert = $cer.GetCertHash()
-$base64Thumbprint = [System.Convert]::ToBase64String($binCert)
-Write-Host '[verbose] Thumbprint of certificate: '$cer.Thumbprint
+If (-not (Test-Path $Path)) 
+{
+    throw [System.IO.FileNotFoundException] "Certificate not found"
+}
 
-$start = [datetime]::ParseExact([string]$cer.GetEffectiveDateString(), 'dd-MM-yyyy HH:mm:ss', $null)
-$end = [datetime]::ParseExact([string]$cer.GetExpirationDateString(), 'dd-MM-yyyy HH:mm:ss', $null)
-$start = $start.ToUniversalTime()
-$end = $end.ToUniversalTime()
-Write-Host '[verbose] Start Date of Certificate in UTC:' $start
-Write-Host '[verbose] End Date of Certificate in UTC:' $end
+$certificate = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 
+$certificate.Import($Path, $Password, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::PersistKeySet) 
+$binCert = $certificate.GetRawCertData() 
+$credential = [System.Convert]::ToBase64String($binCert)
+
+Write-Host '[verbose] Certificate.Thumbprint:' $certificate.Thumbprint
+
+$start = [datetime]::ParseExact([string]$certificate.GetEffectiveDateString(), 'dd-MM-yyyy HH:mm:ss', $null).ToUniversalTime()
+$end = [datetime]::ParseExact([string]$certificate.GetExpirationDateString(), 'dd-MM-yyyy HH:mm:ss', $null).ToUniversalTime()
+
+Write-Host '[verbose] Certificate.StartDate (UTC):' $start
+Write-Host '[verbose] Certificate.EndDate (UTC):' $end
 
 Write-Host '[verbose] Searching for AAD resource using App ID: "'$ApplicationId'"'
 $resource = Get-AzureRmADApplication -ApplicationId $ApplicationId
@@ -46,7 +50,7 @@ Write-Host [verbose] $creds.Count certificate found with matching End Date
 If ( $creds.Count -eq 0 ) 
 { 
     Write-Host [information] Upload certificate to Azure AD resource
-    New-AzureRmADAppCredential -ApplicationId $ApplicationId -CertValue $credValue -StartDate $start -EndDate $end 
+    New-AzureRmADAppCredential -ApplicationId $ApplicationId -CertValue $credential -StartDate $start -EndDate $end 
 }
 else 
 {
@@ -56,4 +60,3 @@ else
 $credentials = Get-AzureRmADAppCredential -ApplicationId $ApplicationId | Where-Object { $_.Type -eq 'AsymmetricX509Cert' }
 Write-Host '[verbose] Total Certificates present: ' $credentials.Count
 $credentials
-
